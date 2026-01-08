6e9dc305d95c10b8334e0a67bd144084f8c5d26f
Gede Pradnyananda <gedenanda4444@gmail.com>
2026-01-08 23:41:41 +0700

    fix: update baseURL configuration for API to use empty string for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // Kalau mode Production (Vercel), pakai '' (relative path).
-    // Kalau mode Development (Laptop), pakai 'http://localhost:5000'
-    baseURL: import.meta.env.PROD ? '' : 'http://localhost:5000',
+    // KITA PAKSA KOSONG DULU BIAR JALAN DI VERCEL
+    baseURL: '',
+
+    // Script lama (komentari dulu):
+    // baseURL: import.meta.env.PROD ? '' : 'http://localhost:5000',
+
     withCredentials: true
 });
 
